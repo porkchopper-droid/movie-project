ee12a533e8e78f77a59d096c588a455894a38899
Dmytro Kuzyk <dmytro.kuzyk@dci-student.org>
2025-02-12 19:56:50 +0100

    HookContext (#9)
    * feat: add homepage's movie based on time of the day
    
    * feat: hook useContext to HomePage
    
    * Merge branch 'main' of github.com:porkchopper-droid/movie-project

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,42 @@ import MovieDetails from "./components/MovieDetails";
 import SignIn from "./components/SignIn";
 import TopRated from "./components/TopRated";
 import SearchPage from "./components/SearchPage";
+import SearchContextProvider from "./contexts/SearchContext";
 import "./App.scss";
 
 
 function App() {
-
   return (
-    <BrowserRouter basename="/movie-project">
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/favorites">Favorites</Link>
-        <Link to="/top-rated">Top Rated</Link>
-        <Link to="/genres">Genres (aka. Magic wheel)</Link>
-        <input type="text" placeholder="search for movies"></input>
-        <button>Search</button>
-        <Link to="/sign-in">Sign in</Link>
-      </nav>
+    <SearchContextProvider>
+      <BrowserRouter basename="/movie-project">
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/favorites">Favorites</Link>
+          <Link to="/top-rated">Top Rated</Link>
+          <Link to="/genres">Genres (aka. Magic wheel)</Link>
+          <input type="text" placeholder="search for movies"></input>
+          <button>Search</button>
+          <Link to="/sign-in">Sign in</Link>
+        </nav>
 
-      <Routes>
-        <Route path="/" element={<HomePage></HomePage>}></Route>
-        <Route path="/favorites" element={<Favorites></Favorites>}></Route>
-        <Route path="/top-rated" element={<TopRated></TopRated>}></Route>
-        <Route path="/genres" element={<Genres></Genres>}></Route>
-        <Route path="/sign-in" element={<SignIn></SignIn>}></Route>
-        <Route path="/movie/:id" element={<MovieDetails></MovieDetails>}></Route>
-        <Route path="/search-page" element={<SearchPage></SearchPage>}></Route>
-        <Route path="*" element={<HomePage></HomePage>}></Route>
-      </Routes>
-    </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage></HomePage>}></Route>
+          <Route path="/favorites" element={<Favorites></Favorites>}></Route>
+          <Route path="/top-rated" element={<TopRated></TopRated>}></Route>
+          <Route path="/genres" element={<Genres></Genres>}></Route>
+          <Route path="/sign-in" element={<SignIn></SignIn>}></Route>
+          <Route
+            path="/movie/:id"
+            element={<MovieDetails></MovieDetails>}
+          ></Route>
+          <Route
+            path="/search-page"
+            element={<SearchPage></SearchPage>}
+          ></Route>
+          <Route path="*" element={<HomePage></HomePage>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </SearchContextProvider>
   );
 }
 
