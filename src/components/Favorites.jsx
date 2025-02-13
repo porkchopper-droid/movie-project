95d050fe4bbf527bc3f34a92d5d30007e928053c
Ahmad Al Sayed <ahmad.alsayed@dci-student.org>
2025-02-13 11:34:34 +0100

    modified favorite component (#11)

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import { useContext } from "react";
+import { SearchContext } from "../contexts/SearchContext";
 
 export default function Favorites() {
+  const { favoritesMovies, setFavoritesMovies } = useContext(SearchContext);
+
+  //Filtirig movies and remove  it from favorite by clicking on the button
+  const removeFromFavorite = (movie) => {
+    const filtringMovies = favoritesMovies.filter((m) => m.imdbID !== movie.imdbID);
+    setFavoritesMovies(filtringMovies);
+  };
+
   return (
-    <div>Favorites</div>
-  )
+    <div className="favorites-container">
+      <h1>{favoritesMovies.length === 0 && " No Favorite movies available"}</h1>
+      <ul className="moviesContainer">
+        {favoritesMovies.map((movie) => (
+          <div className="movieContainer" key={movie.imdbID}>
+            <h4>{movie.Title}</h4>
+            <img
+              src={movie.Poster !== "N/A" ? movie.Poster : "NO IMAGE"}
+              alt={movie.Title + " Poster"}
+              style={{ width: "80%", objectFit: "cover", padding: "10px 0" }}
+            />
+            <p>Year: {movie.Year}</p>
+            <p>Genre: {movie.Genre || "N/A"}</p>
+            <p>Rating: {movie.imdbRating || "N/A"}</p>
+            <button onClick={() => removeFromFavorite(movie)}>
+              Remove from favorite
+            </button>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
 }
