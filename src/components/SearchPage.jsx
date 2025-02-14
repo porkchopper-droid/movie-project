c83d4a5b736c09c9618928274ec56e834e254ba0
Ahmad <ahmad.alsayed@dci-student.org>
2025-02-14 12:03:27 +0100

    solve conflict

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import {useContext} from 'react'
+import { SearchContext } from '../contexts/SearchContext';
 
-export default function SearchPage() {
+
+export default function SearchPage({searchMovies}) {
+  console.log(searchMovies);
+  
+
+  const {addingToFavorite,favoritesMovies,setFavoritesMovies,} = useContext(SearchContext);
+
   return (
-    <div>SearchPage</div>
+    <div>
+    
+    {favoritesMovies.length > 0 && <span>{favoritesMovies.length}</span>}
+
+    <ul className="moviesContainer">
+      {searchMovies.length >0?searchMovies.map((movie) => (
+        <div className="movieContainer" key={searchMovies.imdbID}>
+          <h4>{movie.Title}</h4>
+          <div className="expand">
+            <img
+              src={movie.Poster !== "N/A" ? movie.Poster : "NO IMAGE"}
+              alt={movie.Title + " Poster"}
+              style={{
+                width: "80%",
+                objectFit: "cover",
+                padding: "10px 0",
+                cursor: "pointer",
+              }}
+              onClick={() => navigate(`/movie/${movie.imdbID}`)}
+            />
+          </div>
+          <p>Year: {movie.Year}</p>
+          <p>Genre: {movie.Genre || "N/A"}</p>
+          <p>Rating: {movie.imdbRating || "N/A"}</p>
+          <button onClick={() => addingToFavorite(movie)}>
+            Add to favorite
+          </button>
+        </div>
+      ))
+      :<h1> Sorry There is no Movies in This Name  </h1>}
+    </ul>
+  </div>
   )
 }
--- src/contexts/SearchContext.jsx
+++ src/contexts/SearchContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from "react";
+import  { createContext, useState } from "react";
 
 export const SearchContext = createContext();
 
@@ -6,7 +6,8 @@ export default function SearchContextProvider(props) {
   const [movies, setMovies] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [favoritesMovies, setFavoritesMovies] = useState([]);
-
+  const [searchMovies,setSearchMovies]=useState([])
+  
   function handleSearch(query) {
     const myAPIkey = "4a822498";
     fetch(`https://www.omdbapi.com/?apikey=${myAPIkey}&s=${query}`)
@@ -14,6 +15,18 @@ export default function SearchContextProvider(props) {
       .then((data) => setMovies(data.Search || []))
       .catch((error) => console.error("Error fetching movies:", error));
   }
+   
+
+
+  
+  const addingToFavorite = (movie) => {
+    if (!favoritesMovies.includes(movie)) {
+      setFavoritesMovies((prev) => [...prev, movie]);
+      console.log(favoritesMovies);
+    } else {
+      console.log("movie already exist");
+    }
+  };
 
   return (
     <SearchContext.Provider
@@ -24,6 +37,10 @@ export default function SearchContextProvider(props) {
         setSearchQuery,
         movies,
         handleSearch,
+        setMovies,
+        searchMovies,
+        setSearchMovies,
+        addingToFavorite
       }}
     >
       {props.children}
