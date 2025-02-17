7e2787ad1840a2d79259118a5af5b3672edf26d7
Ahmad <ahmad.alsayed@dci-student.org>
2025-02-17 09:52:24 +0100

    fixing Errors

--- src/App.jsx
+++ src/App.jsx
@@ -73,38 +73,7 @@ function App() {
       </Routes>
     </BrowserRouter>
 
-    <SearchContextProvider>
-      <BrowserRouter basename="/movie-project">
-        <nav>
-          <Link to="/">Home</Link>
-          
-          <Link to="/favorites">Favorites</Link>
-          <Link to="/top-rated">Top Rated</Link>
-          <Link to="/genres">Genres (aka. Magic wheel)</Link>
-          <input type="text" placeholder="search for movies"></input>
-          <button>Search</button>
-          <Link to="/sign-in">Sign in</Link>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<HomePage></HomePage>}></Route>
-          <Route path="/favorites" element={<Favorites></Favorites>}></Route>
-          <Route path="/top-rated" element={<TopRated></TopRated>}></Route>
-          <Route path="/genres" element={<Genres></Genres>}></Route>
-          <Route path="/genres/:genre" element={<GenreMovies />} />
-          <Route path="/sign-in" element={<SignIn></SignIn>}></Route>
-          <Route
-            path="/movie/:id"
-            element={<MovieDetails></MovieDetails>}
-          ></Route>
-          <Route
-            path="/search-page"
-            element={<SearchPage></SearchPage>}
-          ></Route>
-          <Route path="*" element={<HomePage></HomePage>}></Route>
-        </Routes>
-      </BrowserRouter>
-    </SearchContextProvider>
+    
 
   );
 }
